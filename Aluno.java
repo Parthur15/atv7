1120fdb2686828fcf2bc0144ee420f691960350b
ARTHUR ALBINO PIRES <100172812+Parthur15@users.noreply.github.com>
2022-04-18 21:46:12 -0300

    Atividade.7
    Co-Authored-By: Caio Vasconcelos <100734526+vasconceloscaio@users.noreply.github.com>

---
+++ Aluno.java
@@ -0,0 +1,40 @@
+/**
+ *
+ * @vasconceloscaio 
+ */
+public class Aluno {
+
+    private String m1;
+    private String m2;
+    private float media;
+
+   
+
+    public void calc_media() {
+        float m1 = (float) (new Float(this.m1).floatValue());
+        float m2 = (float) (new Float(this.m2).floatValue());
+        this.media = ((m2 * 2) + m1) / 3;
+
+    }
+
+    public String getM1() {
+        return m1;
+    }
+
+    public void setM1(String m1) {
+        this.m1 = m1;
+    }
+
+    public String getM2() {
+        return m2;
+    }
+
+    public void setM2(String m2) {
+        this.m2 = m2;
+    }
+
+    public float getMedia() {
+        return media;
+    }
+    
+}
